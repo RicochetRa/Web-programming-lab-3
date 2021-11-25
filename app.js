1d2ca134b6c462b2f46d4065c1ad87052cbfb12d
danielle <danielle@DESKTOP-MUS0S62.localdomain>
2021-11-25 13:49:04 +0000

    completed all but final parts

--- app.js
+++ app.js
@@ -1,14 +1,43 @@
 const express = require('express');
+//var mysql = require('mysql');
 
 //creating app
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("Hello, your server is up and running");
+    res.sendFile('index.html', {root:__dirname});
 });
+
 
 //make the app listen on port
 const port = process.argv[2] || process.env.PORT || 3000;
 const server = app.listen(port, () => {
     console.log(`Cart app listening at http://localhost:${port}`);
 });
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+//route for contacts
+app.get('/contacts', (req, res) => {
+    res.render('contacts');
+});
+
+//for register
+app.get('/clients', (req, res) => {
+    res.render('register');
+});
+
+//for login
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+const router = require('./routes/apis');
+app.use(router);
+
+
